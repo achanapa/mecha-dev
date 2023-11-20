2b4d57e265e0ddf2afc41a5df365cdf47b5e11f3
aox <85739324+Tunyalucklie@users.noreply.github.com>
2023-11-20 18:50:30 +0700

    Update ScrewyDisplay.js

--- client/src/ScrewyDisplay.js
+++ client/src/ScrewyDisplay.js
@@ -1,20 +1,17 @@
 import "./ScrewyDisplay.css";
 import axios from 'axios';
-import React,{ useRef,useEffect, useState} from 'react';
+import React,{ useRef, useState} from 'react';
 import ModelViewer from './ModelViewer';
+import image from './pinbg.jpg'
 
 const ScrewyDisplay = () => {
 
   //usestate for camera status
   const [isCheckOn, setIsCheckOn] = useState('off');
-  const previousCheckOn = useRef("");
-
-  useEffect(() => {
-    previousCheckOn.current = isCheckOn;
-  }, [isCheckOn]);
-
 
   const isPylonOn = async () => {
+
+    const status_next = ''
    
     axios
       .post("http://127.0.0.1:5000/publish", {msg:'isOn'})
@@ -25,31 +22,38 @@ const ScrewyDisplay = () => {
       .catch((error) => {
         // Handle any errors that occur during the request
         console.error("Error:", error);
-        setIsSendType(false);
+
       });
     
     try {
       const response_status = await axios.get('http://127.0.0.1:5000/get_status');
-      setIsCheckOn(response_status.data['msg']);
+      const status_next = response_status.data['msg'];
       console.log(response_status.data)
+      setIsCheckOn(status_next)
+
     } catch (error) {
       console.error('Error fetching data:', error);
     }
     
-    if ( isCheckOn == 'on') {
+    if (isCheckOn == 'on') {
       //openPylonbutton
       alert('The device is ready');
     } else if (isCheckOn == 'off') {
-      alert('You have not connect to the device');
+      alert('The device is now closed');
     } else {
       //closePylonbutton
-      alert('The device is now closed');
+      alert('You have not connect to the device');
     };
   };
 
   //usestate for taking photo 
-   const [dataPic, setdataPic] = useState('');
+   const [dataPic, setdataPic] = useState({});
+   const [showscrew2d, setshowscrew2d] = useState(false);
+
    const takingPic = async () => {
+
+    setshowscrew2d(current => true);
+
     axios
     .post("http://127.0.0.1:5000/publish", {msg:'isTaken'})
     .then((response_pic) => {
@@ -59,7 +63,7 @@ const ScrewyDisplay = () => {
     .catch((error) => {
       // Handle any errors that occur during the request
       console.error("Error:", error);
-      setIsSendType(false);
+
     });
 
     try {
@@ -78,16 +82,20 @@ const ScrewyDisplay = () => {
 
   };
   
-  //confirm
-  const [dataResult, setdataResult] = useState('');
+  //confirm picture to processed
+
+  const [ choosedScrew2d, setChoosedScrew2d] = useState(false);
+
   const [dataNom, setdataNom] = useState('');
   const [dataH, setdataH] = useState('');
   const [dataK, setdataK] = useState('');
-  const [dataL, setdataL] = useState('');
   const [dataTL, setdataTL] = useState('');
   const [dataSL, setdataSL] = useState('');
 
   const confirmPic = async () => {
+
+    setChoosedScrew2d(current => true);
+   
     axios
     .post("http://127.0.0.1:5000/publish", {msg:'isProcessed'})
     .then((response_result) => {
@@ -116,8 +124,11 @@ const ScrewyDisplay = () => {
  
   };
 
-  //retake
+  //retake photo - clear all infomation
   function reTake() {
+    setshowscrew2d(current => false);
+    setChoosedScrew2d(current => false);
+    //setIsLoading3D(current => false);
     setdataPic('')
     setdataNom('')
     setdataH('')
@@ -128,14 +139,13 @@ const ScrewyDisplay = () => {
   };
 
   // use state to exporting to 
-  const [data3D, setData3D] = useState({});
-  const [isSendType, setIsSendType] = useState(false);
-  const [isLoading3D, setIsLoading3D] = useState(false);
+  const [data3D, setData3D] = useState('');
   const [err, setErr] = useState('');
+
 
   // Add a function for exportButton to send the POST request ans get GET request
   const exportClick = async () => {
-    setIsSendType(true);
+
     // Get the user selections from the dropdowns
     const selectedHead = document.querySelector(".head-select select");
     const selectedBit = document.querySelector(".bit-select select");
@@ -147,84 +157,87 @@ const ScrewyDisplay = () => {
       type_bit: selectedBit.options[selectedBit.selectedIndex].text,
     };
     // Send a POST request to the backend
+    
     axios
       .post("http://127.0.0.1:5000/combine_and_store_data", requestData)
       .then((response) => {
         // Handle the response from the server
-        setIsSendType(false);
+        
       })
       .catch((error) => {
         // Handle any errors that occur during the request
         console.error("Error:", error);
-        setIsSendType(false);
+       
       });
 
-    try {
-        const response_glink = await axios.get('http://127.0.0.1:5000/get_GoogleLink');
-        console.log('get_data',response_glink.data);
-        setData3D(response_glink.data['link'])
+      try {
+          const response_glink = await axios.get('http://127.0.0.1:5000/get_GoogleLink');
+          console.log('get_data',response_glink.data);
+          setData3D(response_glink.data['link'])
+
       } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+          console.error('Error fetching data:', error);
+      } 
 
     };
 
     //click see result process
-
     const screwy_3d = document.querySelector(".three_d");
     const [showscrew3d, setshowscrew3d] = useState(false);
-    
     const Showscrew3d = event => {
        setshowscrew3d(current => !current);
+    }
     
-      };
       
 
   return (
-    <div className="plugin-file-cover-1">
+    <div className="plugin-file-cover-1" >
       <div className="frame-parent">
-        <div className="frame-child" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <div className="frame-child" >
+        {choosedScrew2d && (<img className= 'showphoto2' src={`data:image/jpeg;base64, ${dataPic}`} alt=""/>)}
+        </div>
       </div>
-      <div className="aboutUs">About Us</div>
+      <div className="aboutUs">
+        <b></b>
+      </div>
       <div className="choosePicture" >
-        <img className= 'showphoto' src={`data:image/jpeg;base64, ${dataPic}`} alt=""/>
+        {showscrew2d && (<img className= 'showphoto' src={`data:image/jpeg;base64, ${dataPic}`} alt=""/>)}
       </div>  
       <div className="show3D"> 
         <button className="clicksee" onClick={Showscrew3d}>show 3D model</button>
         {showscrew3d && (
-         <ModelViewer  scale={0.1} modelPath={'../Bolt_ID38.glb'} className='three_d' />
+         <ModelViewer  scale={0.1} modelPath={'../temp/downloaded.glb'} className='three_d' />
       )}
-      </div>
+      </div> 
       <div className="toImage">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-        <button className="checkOn" onClick={isPylonOn} ></button>
+        <button className="checkOn" onClick={isPylonOn} ><i className="fa fa-power-off fa-2x" aria-hidden="true"></i></button>
         <button className="takePhoto" onClick={takingPic} ><i className="fa fa-camera fa-2x" aria-hidden="true"></i></button>
-        <button className="confirmImage" onClick={confirmPic}></button>
-        <button className="retakePhoto" onClick={reTake} ></button>
+        <button className="confirmImage" onClick={confirmPic}><i className="fa fa-check-circle fa-2x" aria-hidden="true"></i></button>
+        <button className="retakePhoto" onClick={reTake} ><i className="fa fa-times-circle fa-2x" aria-hidden="true"></i></button>
       </div>
       <div className="showScrewInfo" >
         <div className="identifyTo">Screw Identifier</div>
         <div className="convertingTo">Converting 2D to 3D</div>
       </div>
-      <div className="dimension">
-        <div className= "nom">M {dataNom} </div>
-        <div className = "h">{dataH} mm.</div>
-        <div className = "k">{dataK} mm.</div>
-        <div className = "l">{dataL} mm.</div>
-        <div className = "tl">{dataTL} mm.</div>
-        <div className = "sl">{dataSL} mm.</div>
+      <div className="dimension"> 
+        <div className= "nom">{ choosedScrew2d && (<b>M {dataNom}</b>)}</div>
+        <div className = "h">{ choosedScrew2d && (<b>{dataH} mm.</b>)}</div>
+        <div className = "k">{ choosedScrew2d && (<b>{dataK} mm.</b>)}</div>
+        <div className = "tl">{ choosedScrew2d && (<b>{dataTL} mm.</b>)}</div>
+        <div className = "sl">{ choosedScrew2d && (<b>{dataSL} mm.</b>)}</div>
       </div>
       <div className="load">
         <button className="exportingTo" onClick={exportClick} >Export to 3D model</button>
       </div>
       <div className="loadTol">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-        <b className="clickto">click below to download</b>
-        <a className="Download" href={data3D} target="_blank"><i className="fa fa-download"></i> Download</a>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+              <b className="clickto">click below to download</b>
+              <a className="Download" target="_blank" href={data3D} ><i className="fa fa-download"></i> Download</a>
       </div>
       
       <div className="custom-select-frame">
+        <link href='https://fonts.googleapis.com/css?family=Alef' rel='stylesheet'/>
         <div className="head-select" style = {{width: "100px"}}>
                 <select>
                     <option value="0">Select head:</option>
@@ -253,14 +266,19 @@ const ScrewyDisplay = () => {
         <div className ="size">Nominal Size</div>
         <div className ="headDia">Head Diameter (H) </div>
         <div className ="headHeight">Height of Head (K) </div>
-        <div className ="blen">Bolt Length</div>
         <div className ="tlen">Thread Length</div>
-        <div className ="slen">Space Length</div>
+        <div className ="slen">Grid Length</div>
       </div>
     </div>
+   
   );
 };
 
 
 export default ScrewyDisplay;
+
+//       <div className = "l">{ choosedScrew2d && (<b>{dataL} mm.</b>)}</div>
+//        <div className ="blen">Bolt Length</div>
+//  <div> className='picbg'style={{ backgroundImage:`url(${image})` ,backgroundRepeat:"no-repeat"}}</div>
+
 
