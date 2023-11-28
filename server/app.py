a20a21a3e2198785a81ee0431ba7a375a8f9483f
achanapa <116539286+achanapa@users.noreply.github.com>
2023-11-28 16:10:21 +0700

    Update app.py

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from pymongo import MongoClient
 import time
      
 app = Flask(__name__)
-client = MongoClient('mongodb+srv://mechatronics:BhamAomNunEarn@dimension.i10gagw.mongodb.net/')  # Adjust MongoDB connection details
+client = MongoClient('mongoURI')
 CORS(app)
 
 # Define MongoDB collections
@@ -21,12 +21,12 @@ Captured = db["Captured"]
 
 
 #mqtt
-app.config['MQTT_BROKER_URL'] = '161.200.84.240'  # broker 
-app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
-app.config['MQTT_USERNAME'] = 'cps'  # set the username here if you need authentication for the broker
-app.config['MQTT_PASSWORD'] = '21035350'  # set the password here if the broker demands authentication
-app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
-app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+app.config['MQTT_BROKER_URL'] = '161.200.84.240'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'cps'
+app.config['MQTT_PASSWORD'] = 'password'
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
     
 mqtt = Mqtt(app) 
 topic_pub = '/mqtt/toPi1'
